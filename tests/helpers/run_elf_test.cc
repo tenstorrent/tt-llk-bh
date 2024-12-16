bf074f49163f46f882d3205e66da9d5d4af703e1
ldjurovicTT <ldjurovic@tenstoreent.com>
2024-12-16 16:34:53 +0000

    Trying to fix sfpu for Float16_a

--- tests/helpers/run_elf_test.cc
+++ tests/helpers/run_elf_test.cc
@@ -29,6 +29,26 @@
 	#endif
 #endif
 
+
+// inline void c_tensix_core::ex_load_const(vptr_uint instrn_buf) {
+//     // Load LREG11 w/ -1.0f by convention
+//     uint instrn;
+//     instrn = (0xbf80 << 0);  // Load LREG0 w/ -1.0f
+//     ex_push_insn(instrn_buf, INSTRN_SFPLOADI(instrn));
+//     instrn = (11 << 4);  // Set LREG11 to LREG0
+//     ex_push_insn(instrn_buf, INSTRN_SFPCONFIG(instrn));
+// }
+
+// core.ex_rmw_cfg(0, ECC_SCRUBBER_Enable_RMW, 1);
+// core.ex_rmw_cfg(0, ECC_SCRUBBER_Scrub_On_Error_RMW, 1);
+// core.ex_rmw_cfg(0, ECC_SCRUBBER_Delay_RMW, 0x100);
+
+// inline void ex_rmw_cfg(uint cfg_addr32, uint cfg_shamt, uint32_t cfg_mask, uint wr_val, vptr_uint cfg_regs) {
+//     uint addr = cfg_addr32;
+//     uint32_t cfg_data = cfg_regs[addr];
+//     cfg_regs[addr] = rmw_cfg_value(cfg_shamt, cfg_mask, wr_val, cfg_data);
+// }
+
 int main()
 {
     //FWEVENT("Launching proudction env kernels");
@@ -37,7 +57,9 @@ int main()
 	reset_dest_offset_id();
 
 	#ifdef LLK_TRISC_MATH
-    TT_ZEROACC(p_zeroacc::CLR_ALL, 0, 0, 1, 0);
+    TTI_ZEROACC(p_zeroacc::CLR_ALL, 0, 0, 1, 0);
+	TTI_SFPENCC(3,0,0,10);
+	TTI_NOP;
 	#endif
 
 	#ifdef MULTIPLE_OPS
