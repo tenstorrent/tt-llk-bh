9c213cf8466e9c2bb43fb1372fafc09939d76cc2
ldjurovicTT <ldjurovic@tenstoreent.com>
2025-01-27 16:14:07 +0000

    Option for const faces in stimuli_generator and Makefile fix

--- tests/python_tests/helpers/stimuli_generator.py
+++ tests/python_tests/helpers/stimuli_generator.py
@@ -4,14 +4,16 @@ from .dictionaries import *
 def flatten_list(sublists):
     return [item for sublist in sublists for item in sublist]
 
-def generate_random_face(a , stimuli_format = "Float16_b"):
+def generate_random_face(stimuli_format = "Float16_b", const_face = 3):
 
     if(stimuli_format == "Float16" or stimuli_format == "Float16_b"): 
-        srcA_face = torch.rand(256, dtype = format_dict[stimuli_format]) + 2 # because of log
-        # if a == 1:
-        #     srcA_face = torch.ones(256, dtype = format_dict[stimuli_format]) * 1
-        # else:
-        #     srcA_face = torch.ones(256, dtype = format_dict[stimuli_format]) * 3
+        #srcA_face = torch.rand(256, dtype = format_dict[stimuli_format]) + 2 # because of log
+        if const_face == 0: # value for source A
+            srcA_face = torch.ones(256, dtype = format_dict[stimuli_format]) * 1
+        elif const_face == 1: # const value for source B
+            srcA_face = torch.ones(256, dtype = format_dict[stimuli_format]) * 3
+        else: # random for both faces
+            srcA_face = torch.rand(256, dtype = format_dict[stimuli_format]) + 2 # because of log
     elif(stimuli_format == "Bfp8_b"):
         size = 256
         integer_part = torch.randint(0, 2, (size,))  
@@ -24,16 +26,16 @@ def generate_random_face(a , stimuli_format = "Float16_b"):
 
     return srcA_face
 
-def generate_random_face_ab(stimuli_format):
-    return generate_random_face(1,stimuli_format), generate_random_face(0,stimuli_format)
+def generate_random_face_ab(stimuli_format, const_face = 3):
+    return generate_random_face(stimuli_format,const_face), generate_random_face(stimuli_format,const_face)
 
-def generate_stimuli(stimuli_format = "Float16_b", tile_cnt = 1, sfpu = False):
+def generate_stimuli(stimuli_format = "Float16_b", tile_cnt = 1, sfpu = False, const_face = 3):
 
     srcA = []
     srcB = []
 
     for i in range(4*tile_cnt):
-        face_a, face_b = generate_random_face_ab(stimuli_format)
+        face_a, face_b = generate_random_face_ab(stimuli_format, const_face)
         srcA.append(face_a.tolist())
         srcB.append(face_b.tolist())
 
